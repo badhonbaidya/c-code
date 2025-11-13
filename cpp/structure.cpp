48d18c878d48e90c24412eddc8fb48a4897d6bb3
badhonbaidya <150837730+badhonbaidya@users.noreply.github.com>
2025-11-13 19:35:12 +0600

    Add files via upload

--- cpp/structure.cpp
+++ cpp/structure.cpp
@@ -1,22 +1,9 @@
-#include <stdio.h>
-
-struct student
-{
-
-    int id;
-    float cgpa;
-    char name[20];
-};
+#include<stdio.h>
 
 int main(){
-    struct student s;
-    printf("enter your id :\n");
-    scanf("%d",&s.id);
-    printf("enter your cgpa :\n");
-    scanf("%f", &s.cgpa);
-    printf("enter your name :\n");
-    scanf("%s", s. name);
-    printf("name :%s;id: %d;cgpa:%f\n", s.name, s.id, s.cgpa);
+
+    printf("badhon");
+
 
     return 0;
 }
