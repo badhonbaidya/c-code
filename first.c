89d03023bdead952cad85751f796d5998e53cb76
badhonbaidya <150837730+badhonbaidya@users.noreply.github.com>
2026-03-18 20:37:38 +0600

    Add files via upload

---
+++ first.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main ()
+
+{
+ 
+
+  
+// int number_1;
+// printf("number is the: %d",number_1);
+
+printf("size is the my computer : %d",sizeof(int));
+
+
+
+return 0;
+
+}
