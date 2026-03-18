62cd3dbdff0f4b9bbbcd7e761d186de77ff68bcc
badhonbaidya <150837730+badhonbaidya@users.noreply.github.com>
2026-03-18 20:34:28 +0600

    Add files via upload

---
+++ asch_valu_8.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+// #include<limits.h>
+
+int main ()
+
+{
+
+
+// char x = 'B';
+// printf("%d",x);
+
+int a = 65, b=76, c=66;
+printf("%c%c%c",a,b,c);
+ 
+
+
+
+return 0;
+
+}
